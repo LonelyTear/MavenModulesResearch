b85d0afe130ad88424143ed795048ca78012e845
Jinjb <359920692@qq.com>
2019-06-29 20:14:58 +0800

    addall

---
+++ demo-main/src/main/java/com/example/MainEntrance.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainEntrance {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainEntrance.class, args);
+    }
+}
